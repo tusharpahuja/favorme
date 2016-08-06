56ba96705673abdaaf2aaa7ac6f6bb0d61fc1058
aman_saha <aman.saha2015@vit.ac.in>
2016-08-07 04:49:29 +0530

    post.php working

--- public/post.php
+++ public/post.php
@@ -1,3 +1,8 @@
+<?php
+include("../includes/functions.php");
+include("../includes/session.php");
+include("../includes/database_connection.php");
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -5,11 +10,18 @@
 	</head>
 	<body>
 		<form action="post.php" method="GET">
-			<div id="<?php echo $_GET['favor_id']; ?>">
+			<div id="<?php echo $_GET['favor_id'];?>">
 				<?php
+					$favor_id=$_GET['favor_id'];
 					$category=$_GET['category'];
 					echo "$category";
-					echo $_GET['favor_id'];
+					echo $favor_id;
+					$query="SELECT * FROM academics WHERE favor_id= $favor_id LIMIT 1";
+					$result=mysqli_query($connection,$query);
+					if($result){
+						$row=mysqli_fetch_assoc($result);
+						echo $row['favor'];
+					}
 				?>
 			</div>
 		</form>
